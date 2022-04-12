169408f0ed41181f0cbcbf2d350c1859f1f8fe44
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-04-12 14:41:46 +0530

    Update Validation.cs

--- RegularExpressionAssignment/Validation.cs
+++ RegularExpressionAssignment/Validation.cs
@@ -72,12 +72,13 @@ namespace RegularExpressionAssignment
                 return false;
             }
         }
-
-        public static bool PasswordValidation(string password) // Creating a method for password validation
+        public static bool PasswordRule1Validation(string password) // Creating a method for password validation
         {
-            string pattern = "^(?=.*[A-Z])(?=.{8,})"; // Regex for password validation up to rule 2
+            //Rule -1 Minimum 8 characters
+            string patternForRule1 = "^[0-9a-zA-Z@#$%^&*!+=]{8,}$"; // Regex for password validation rule 1
+            
             //If password entered by user is match with regex then it is valid otherwise not
-            if (Regex.IsMatch(password, pattern))
+            if (Regex.IsMatch(password, patternForRule1))
             {
                 Console.WriteLine($"\nYour password \"{password}\" is valid");
                 return true;
@@ -88,7 +89,22 @@ namespace RegularExpressionAssignment
                 return false;
             }
         }
-
+        public static bool PasswordRule2Validation(string password) // Creating a method for password validation
+        {
+            //Rule - 2 Atleast 1 Capital Letter
+            string patternForRule2 = "^(?=.*[A-Z])[0-9a-zA-Z@#$%^&*!+=]{8,}$"; // Regex for password validation rule 2
+            //If password entered by user is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(password, patternForRule2))
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is valid");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour password \"{password}\" is not follow password rules");
+                return false;
+            }
+        }
     }
 }
 
